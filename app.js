e2f3db65b91bd31498d53438f55b0dc20cd6d2f6
NoLimitIsTheBest <118146914+NoLimitIsTheBest@users.noreply.github.com>
2023-03-02 17:22:04 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+function initialize(){
+    var state = "*Offline*"
+    if(navigator.onLine){
+        status = "*Online*";
+        retrieveContacts()
+    }else{
+        const localStorage = window.localStorage;
+        if(localStorage){
+            const contacts = localStorage.getItem("contacts");
+            if(contacts){
+                displayContacts(JSON.parse(contacts));
+            }
+        }
+    }
+    document.getElementById("status").innerHTML = status;
+
+    document.body.addEventListener("online",
+    function(){
+        document.getElementById("status").innerHTML="Online";
+    },false);
+
+    document.body.addEventListener(
+        "offline",
+        function () {
+            document.getElementById("status").innerHTML = "Offline";
+        },
+        false
+
+    );
+}
+
+function retrieveContacts(){
+    const xhr = new XMLHttpRequest();
+    const url = "contacts.json"
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            var contacts = JSON.parse(xhr.response).contacts;
+            displayContacts(contacts);
+
+            const localStorage = window.localStorage;
+            if(localStorage){
+                localStorage.setItem("contacts", JSON.stringify(contacts));
+            }
+        }
+
+    };
+
+    xhr.open("get,url");
+    xhr.send();
+
+}
+
+function displayContacts(contacts){
+    contacts.forEach(addRow);
+}
+
+function addRow(contact){
+    var tcontent = document.getElementById("tcontent");
+    var row = tcontent.insertRow();
+
+    var nameCell = row.insertCell();
+    nameCell.setAttribute("data-label", "Name");
+    nameCell.innerHKTML = contact.name;
+
+    var addressCell = row.insertCell();
+    addressCell.setAttribute("data-label", "Address");
+    addressCell.innerHKTML = contact.address;
+
+    var mobileCell = row.insertCell();
+    mobileCell.setAttribute("data-label", "Mobile");
+    mpbileCell.innerHKTML = contact.monbile;
+
+}
